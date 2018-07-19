adf8ae017173b46e401274b4e06296728f44ad22
ablazejo <ablazejo@gmail.com>
2018-07-19 09:53:21 +0200

    ordering

--- Scraping Data/ScrapingData.py
+++ Scraping Data/ScrapingData.py
@@ -1,4 +1,3 @@
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,7 +5,7 @@ base_url = 'https://www.yelp.com/search?find_desc=Restaurants&find_loc='
 loc = 'Newport+Beach,+CA,+United+States'
 current_page = 0
 
-while current_page < 101:
+while current_page < 51:
     print(current_page)
     url = base_url + loc + "&start=" + str(current_page)
     yelp_r = requests.get(url)
@@ -16,33 +15,34 @@ while current_page < 101:
     with open(file_path, "a") as textfile:
         businesses = yelp_soup.findAll('div', {'class': 'biz-listing-large'})
         for biz in businesses:
-            #print(biz)
             title = biz.findAll('a', {'class': 'biz-name'})[0].text
             print(title)
             first_line = ""
             try:
                 address = biz.findAll('address')[0].contents
-                print(address)
-                print("koniec address")
                 for item in address:
                     if "br" in str(item):
                         pass
                     else:
-                        #print(item.strip(" \n\t\r"))
-                        first_line += "\n" + item.strip(" \n")
-                    print("KONIEC JEDNEGO ITEM")
+                        first_line += item.strip(" \n")
                 print(first_line)
             except:
                 pass
-            print('\n')
+            try:
+                city = biz.findAll('span', {'class': 'biz-city'})[0].text
+                print(city)
+            except:
+                pass
             try:
                 phone = biz.findAll('span', {'class': 'biz-phone'})[0].getText().strip(" \n\t\r")
             except:
                 phone = None
             print(phone)
-            page_line = "NAME: {title}\nADDRESS: {address_1}\nPHONE {phone}\n\n".format(
+            print("")
+            page_line = "NAME: {title}\nADDRESS: {address_1}\n{city}\nPHONE: {phone}\n\n".format(
                     title=title,
                     address_1=first_line,
+                    city = city,
                     phone = phone
                 )
             textfile.write(page_line)
