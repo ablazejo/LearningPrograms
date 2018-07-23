4bfb709bf6f953d59fbd3f06b938af03793a7077
ablazejo <ablazejo@gmail.com>
2018-07-23 22:36:12 +0200

    change 3

--- InstagramScraping/InstaScrapeman.py
+++ InstagramScraping/InstaScrapeman.py
@@ -2,9 +2,8 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-#url = 'https://www.instagram.com/p/BVFkvmChgGx/?taken-by=bdolega'
-url = 'https://www.instagram.com/p/Blh6hTVFkIR/?taken-by=daria_9o9_'
-nickname = 'thehighlinetulum'
+url = 'https://www.instagram.com/p/BVFkvmChgGx/?taken-by=bdolega'
+nickname = ''
 
 insta_req = requests.get(url)
 insta_soup = BeautifulSoup(insta_req.text, 'html.parser')
@@ -13,4 +12,6 @@ script = body.find('script')
 raw_string = script.text.strip().replace('window._sharedData =', '').replace(';', '')
 raw_load = json.loads(raw_string)
 metrics = raw_load['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_comment']['edges'][0]['node']['owner']['username']
+#second [0] means number of comment, later it will be variable
 print(metrics)
+#now we can make condition if 'metrics' == 'nickname'
